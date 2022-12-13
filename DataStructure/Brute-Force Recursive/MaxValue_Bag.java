5087d391d1c58365db4c0464ce0edef4ad1bd094
Kaiming-Wei <54682709+Kaiming-Wei@users.noreply.github.com>
2022-12-12 22:25:01 -0500

    Create MaxValue_Bag.java

---
+++ DataStructure/Brute-Force Recursive/MaxValue_Bag.java
@@ -0,0 +1,30 @@
+public class MaxValue_Bag {
+  // Giving 2 arrays, weight and value, each represent an object's weight and value
+  // giving a bag that has a size
+  // return max value of the bag 
+  
+  
+  // recursive from left to right
+  // for each object, selected or not-selected
+    public static int maxValue(int[] weight, int[] value, int bagsize){
+        return process(weight, value, bagsize, 0, 0);
+    }
+    private static int process(int[] weight, int[] value, int bagsize, int i, int bag){
+        if (i == value.length){
+            return 0;
+        }
+        if(weight[i] + bag <= bagsize){
+            int selected = value[i] + process(weight, value, bagsize, i+1, bag+weight[i]);
+            int not_selected = process(weight, value, bagsize, i+1, bag);
+            return Math.max(selected,not_selected);
+        }
+        return process(weight, value, bagsize, i+1, bag);
+    }
+    
+    // test
+    public static void main(String args[]) {
+      int[] weight = new int[]{1,2,3}; 
+      int[] value = new int[]{1,52,50}; 
+      System.out.println(maxValue(weight, value, 3)); // 53
+    }
+}
